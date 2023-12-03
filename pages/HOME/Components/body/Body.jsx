99352e7f37d8e3e34965f2dd6c969380b9910e58
Paula Cano <paulacanoroman@gmail.com>
2023-12-03 21:29:12 +0100

    feat: create and import BodyFeatures

--- pages/HOME/Components/body/Body.jsx
+++ pages/HOME/Components/body/Body.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import style from './Body.module.css'
 import BodyTitle from './bodyTitle/BodyTitle'
+import BodyFeatures from './bodyFeatures/BodyFeatures'
 
 const Body = () => {
   return (
     <>
     <BodyTitle></BodyTitle>
+    <BodyFeatures></BodyFeatures>
     </>
   )
 }
---
+++ pages/HOME/Components/body/bodyFeatures/BodyFeatures.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BodyFeatures = () => {
+  return (
+    <div>BodyFeatures</div>
+  )
+}
+
+export default BodyFeatures
