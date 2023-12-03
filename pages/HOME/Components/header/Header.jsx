0434e0bf7d94dee6c330a316164af2d4d3461c4b
Paula Cano <paulacanoroman@gmail.com>
2023-12-03 22:09:39 +0100

    feat: create and import component HeaderImg

--- pages/HOME/Components/header/Header.jsx
+++ pages/HOME/Components/header/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import style from './Header.module.css'
 import HeaderLogo from './headerLogo/HeaderLogo'
+import HeaderImg from './headerMenu/HeaderImg'
 
 const Header = () => {
   return (
     <div className={style.header}>
       <HeaderLogo></HeaderLogo>
+      <HeaderImg></HeaderImg>
     </div>
   )
 }
