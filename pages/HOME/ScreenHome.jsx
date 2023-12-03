3134e533808c0e6dba591d9ce591758ee653a780
Paula Cano <paulacanoroman@gmail.com>
2023-12-03 21:25:20 +0100

    feat: create and import component Footer

--- pages/HOME/ScreenHome.jsx
+++ pages/HOME/ScreenHome.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import style from './ScreenHome.module.css'
 import Header from './Components/header/Header'
+import Footer from './Components/footer/Footer'
 
 const ScreenHome = () => {
   return (
     <>
     <Header></Header>
     <Body></Body>
+    <Footer></Footer>
     </>
   )
 }
