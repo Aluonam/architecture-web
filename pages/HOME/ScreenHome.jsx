63ad487ea9588b966e041b09713031c76f3a7340
Paula Cano <paulacanoroman@gmail.com>
2023-12-03 21:36:14 +0100

    fix: import properly Body in ScreenHome

--- pages/HOME/ScreenHome.jsx
+++ pages/HOME/ScreenHome.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import style from './ScreenHome.module.css'
 import Header from './Components/header/Header'
 import Footer from './Components/footer/Footer'
+import Body from './Components/body/Body'
 
 const ScreenHome = () => {
   return (
