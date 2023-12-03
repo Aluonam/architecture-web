546c1ec54e8f9a60531c63d588bce2d1ab7843d4
Paula Cano <paulacanoroman@gmail.com>
2023-12-03 21:30:46 +0100

    feat: create and import BodyGallery

--- pages/HOME/Components/body/Body.jsx
+++ pages/HOME/Components/body/Body.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import style from './Body.module.css'
 import BodyTitle from './bodyTitle/BodyTitle'
 import BodyFeatures from './bodyFeatures/BodyFeatures'
+import BodyGallery from './bodyGallery/BodyGallery'
 
 const Body = () => {
   return (
     <>
     <BodyTitle></BodyTitle>
     <BodyFeatures></BodyFeatures>
+    <BodyGallery></BodyGallery>
     </>
   )
 }
---
+++ pages/HOME/Components/body/bodyGallery/BodyGallery.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BodyGallery = () => {
+  return (
+    <div>BodyGallery</div>
+  )
+}
+
+export default BodyGallery
