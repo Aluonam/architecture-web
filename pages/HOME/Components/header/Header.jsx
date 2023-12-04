10273b6efd5ac4860b0b5b15ce88395f9d5bcaf0
Paula Cano <paulacanoroman@gmail.com>
2023-12-04 12:50:40 +0100

    fix: name header menu and stytle

--- pages/HOME/Components/header/Header.jsx
+++ pages/HOME/Components/header/Header.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import style from './Header.module.css'
 import HeaderLogo from './headerLogo/HeaderLogo'
-import HeaderImg from './headerMenu/HeaderImg'
+import HeaderMenu from './headerMenu/HeaderMenu'
 
 const Header = () => {
   return (
     <div className={style.header}>
       <HeaderLogo></HeaderLogo>
-      <HeaderImg></HeaderImg>
+      <HeaderMenu></HeaderMenu>
     </div>
   )
 }
---
+++ pages/HOME/Components/header/headerMenu/HeaderMenu.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import style from './HeaderMenu.module.css'
+
+const HeaderMenu = () => {
+  return (
+    <div className={style.menu}>Headermenu</div>
+  )
+}
+
+export default HeaderMenu
