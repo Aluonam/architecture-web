9b591332ae029417b7a7022db183185f776cb9c1
Paula Cano <paulacanoroman@gmail.com>
2023-12-05 11:03:00 +0100

    feat: style header menu

--- pages/HOME/Components/header/headerMenu/HeaderMenu.jsx
+++ pages/HOME/Components/header/headerMenu/HeaderMenu.jsx
@@ -3,7 +3,12 @@ import style from './HeaderMenu.module.css'
 
 const HeaderMenu = () => {
   return (
-    <div className={style.menu}>Headermenu</div>
+    <div className={style.menu}>
+      <h3>Feature</h3>
+      <h3>Gallery</h3>
+      <h3>Contact</h3>
+    
+    </div>
   )
 }
 
