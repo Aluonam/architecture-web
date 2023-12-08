fc5c89fe4adcb989decf93173f564e996648e220
Paula Cano <paulacanoroman@gmail.com>
2023-12-08 21:26:32 +0100

    feat: img by url

--- pages/HOME/Components/body/bodyTitle/BodyTitle.jsx
+++ pages/HOME/Components/body/bodyTitle/BodyTitle.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import style from './BodyTitle.module.css'
+import imgArchitectureCorner from '../../../images/architectureImage.jpg'
 
 const BodyTitle = () => {
   return (
     <div className={style.bodyTitle}>
-      <img src=''></img>
-    </div>
-    
+      
+    <img className={style.img} src="https://gatopardo.com/wp-content/uploads/2018/10/architecture-3555773_960_720.jpg"></img>
+    </div>  
   )
 }
 
