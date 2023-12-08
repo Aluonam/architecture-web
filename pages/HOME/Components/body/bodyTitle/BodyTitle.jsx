706cad74a10eefb21d1596c13ee5de36842a04e7
Paula Cano <paulacanoroman@gmail.com>
2023-12-08 21:37:24 +0100

    feat: add title text

--- pages/HOME/Components/body/bodyTitle/BodyTitle.jsx
+++ pages/HOME/Components/body/bodyTitle/BodyTitle.jsx
@@ -5,7 +5,8 @@ import imgArchitectureCorner from '../../../images/architectureImage.jpg'
 const BodyTitle = () => {
   return (
     <div className={style.bodyTitle}>
-      
+      <h1 className={style.textTitle}>ARQUITECTURA <br></br>
+      TÉCNICA</h1>
     <img className={style.img} src="https://gatopardo.com/wp-content/uploads/2018/10/architecture-3555773_960_720.jpg"></img>
     </div>  
   )
