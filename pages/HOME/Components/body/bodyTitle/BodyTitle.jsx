cda8473aa080c7e9c48b3abbacb06bc6044d47b6
Paula Cano <paulacanoroman@gmail.com>
2024-08-28 13:04:07 +0200

    feat new project

--- pages/HOME/Components/body/bodyTitle/BodyTitle.jsx
+++ pages/HOME/Components/body/bodyTitle/BodyTitle.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import style from './BodyTitle.module.css'
-import imgArchitectureCorner from '../../../images/architectureImage.jpg'
 
 const BodyTitle = () => {
   return (
-    <div className={style.bodyTitle}>
-      <h1 className={style.textTitle}>ARQUITECTURA <br></br>
-      TÉCNICA</h1>
-    <img className={style.img} src="https://gatopardo.com/wp-content/uploads/2018/10/architecture-3555773_960_720.jpg"></img>
-    </div>  
+    // <div className={style.bodyTitle}>
+      
+    <div className={style.img} >
+      <h1 className={style.textTitle}>
+        <span>ARQUITECTURA</span>
+        <span>TÉCNICA</span>
+      </h1>
+    </div>
+    // </div>  
   )
 }
 
